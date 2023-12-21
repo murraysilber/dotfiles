2838c35e3765d78b52dca6462aad9e9936dfef46
murraysilber <murraysilber@gmail.com>
2023-12-21 15:20:34 +0200

    Added test url to GH script

--- test_bootstrap.sh
+++ test_bootstrap.sh
@@ -4,6 +4,7 @@
 tart list
 tart delete sonoma-1
 tart clone ghcr.io/cirruslabs/macos-sonoma-vanilla:latest sonoma-1
-tart run --dir=myhost:~/ sonoma-1
+tart run --dir=myhost:~/Developer/dotfiles sonoma-1
 
+# url to test in VM
 zsh <(curl -s https://raw.githubusercontent.com/murraysilber/dotfiles/main/bootstrap.sh) --branch main
