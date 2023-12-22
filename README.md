dbbebb2020b105b908796292a3ef2358d28c683d
murraysilber <murraysilber@gmail.com>
2023-12-22 10:07:47 +0200

    updated: Docs on how to install

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# dotfiles
+# My dotfiles repo and automation script to set up a new Mac.
+
+## Do this first
+
+Copy this and execute in the terminal
+
+```sh
+zsh <(curl -s https://raw.githubusercontent.com/murraysilber/dotfiles/main/bootstrap.sh) --branch main
+```
--- test_bootstrap.sh
+++ test_bootstrap.sh
@@ -5,6 +5,3 @@ tart list
 tart delete sonoma-1
 tart clone ghcr.io/cirruslabs/macos-sonoma-vanilla:latest sonoma-1
 tart run --dir=myhost:~/Developer/dotfiles sonoma-1
-
-# url to test in VM
-zsh <(curl -s https://raw.githubusercontent.com/murraysilber/dotfiles/main/bootstrap.sh) --branch main
