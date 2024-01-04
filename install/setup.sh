e5cf26e66991b0acf4aa3a078745c269bdf2ab45
murraysilber <murraysilber@gmail.com>
2024-01-04 15:06:29 +0200

    fix: bundle download location

--- install/setup.sh
+++ install/setup.sh
@@ -9,10 +9,11 @@ curl -L -H "Accept: application/vnd.github.v3+json" --url ${URL} -o setup.zip
 
 unzip setup.zip -d setup
 cd setup/murraysilber-dotfiles*
-echo pwd
+echo "$(pwd -P)"
 
 # move dotfiles folder to home
 CURRENT_WORKING_DIRECTORY=$(pwd -P)
+echo -e "CURRENT_WORKING_DIRECTORY ${CURRENT_WORKING_DIRECTORY} "
 cp -R ${CURRENT_WORKING_DIRECTORY}/dotfiles ${HOME}
 
 #caffeinate zsh ./install.sh "$@"
