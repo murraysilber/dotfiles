48c115de7f1bef29490e52cb18b49df517416ce6
Murray Silber <murraysilber@gmail.com>
2024-03-18 19:28:50 +0200

    feature: rectangle window manager

--- rectangle/setup.sh
+++ rectangle/setup.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
+rectangle_dir="$HOME/.config/rectangle"
+
+if [ ! -d "$rectangle_dir" ]; then
+  echo "$rectangle_dir" does not exist.
+  echo Creating "$rectangle_dir"
+  mkdir -v "$rectangle_dir"
+fi
+
 ln -sf "$PWD/RectangleConfig.json" "$HOME/.config/rectangle/RectangleConfig.json"
+
+brew install --cask rectangle
