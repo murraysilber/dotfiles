61bcdabadb999bbea9ea5ee0aeeed62f0499a9c4
murraysilber <murraysilber@gmail.com>
2024-06-16 21:48:49 +0200

    feature: install and set homebrew zsh as shell

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -12,3 +12,12 @@ ln -sf "$HOME/dotfiles/zsh/.zshenv" "$HOME/.zshenv"
 ln -sf "$HOME/dotfiles/zsh/.zprofile" "$HOME/.config/zsh/.zprofile"
 ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.config/zsh/.zshrc"
 ln -sf "$HOME/dotfiles/zsh/.aliases" "$HOME/.config/zsh/.aliases"
+
+# install hombrew zsh and use
+brew install zsh
+
+# Add the Homebrew zsh to allowed shells
+echo "Changing default shell to Homebrew zsh"
+echo "$(brew --prefix)/bin/zsh" | sudo tee -a /etc/shells >/dev/null
+# Set the Homebrew zsh as default shell
+chsh -s "$(brew --prefix)/bin/zsh"
