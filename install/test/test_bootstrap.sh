646da190d551a6631c06456075b1c5d9bc94e1c0
murraysilber <murraysilber@gmail.com>
2024-09-21 12:55:36 +0200

    feature: test on Sequoia

--- install/test/test_bootstrap.sh
+++ install/test/test_bootstrap.sh
@@ -2,10 +2,10 @@
 
 # clone a new vanilla vm to test bootstrapping a Mac
 tart list
-tart delete sonoma
-tart clone ghcr.io/cirruslabs/macos-sonoma-vanilla:latest sonoma
-tart set sonoma --disk-size 65
-tart run --dir=myhost:~/dotfiles sonoma
+tart delete sequoia
+tart clone ghcr.io/cirruslabs/macos-sequoia-vanilla:latest sequoia
+tart set sequoia --disk-size 65
+tart run --dir=myhost:~/dotfiles sequoia
 
 # keeping this here purely for my testing purposes. I dont want it executed on my host
 
