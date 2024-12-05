2f54f0d36b44f62fb830332c4a35686d5ccead42
Murray Silber <murraysilber@gmail.com>
2024-12-05 15:57:13 +0200

    feature: some very cool keyboard shortcuts for VS Code

--- apps/mise/setup.sh
+++ apps/mise/setup.sh
@@ -13,5 +13,8 @@ ln -sf "$HOME/dotfiles/apps/mise/config.toml" "$mise_dir/config.toml"
 # Install Gleam Plugin
 mise plugins install gleam https://github.com/asdf-community/asdf-gleam
 
+# Install Elixir Plugin
+mise plugin install elixir https://github.com/mise-plugins/mise-elixir.git
+
 # Install default languages from config.toml
 mise install
