492f4a2c49d47f1f980e7e6fdc5814836392a7de
Murray Silber <murraysilber@gmail.com>
2024-12-19 10:48:38 +0200

    feat: added some cool vim shortcut stuff

--- apps/nvim/init.lua
+++ apps/nvim/init.lua
@@ -1,7 +1,26 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+print("Advent of neovim")
+
+
+-- source the current file
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+
+-- source the current line
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+
+-- source selected lines (Visual mode)
+vim.keymap.set("v", "<space>x", ":lua<CR>")
+
+-- Highlight when yanking (copying) text
+-- Try it with yap in Normal mode
+-- See ':help vim.highlight.on._yank()'
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = "highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', {clear = true}),
+  callback = function()
+    vim.highlight.on_yank()
+  end,  
+})
+
 
 vim.o.background = "dark" -- or "light" for light mode
 
