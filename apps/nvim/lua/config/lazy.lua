bf3b537c59bdfe0277e16c02314473ebf988348f
Murray Silber <murraysilber@gmail.com>
2024-12-19 09:07:47 +0200

    feat: added lazy plugin manager for Neovim

--- apps/nvim/lua/config/lazy.lua
+++ apps/nvim/lua/config/lazy.lua
@@ -1,14 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -24,18 +13,20 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
         os.exit(1)
     end
 end
+-- Hey! Put lazy into the runtime path for Neovim
 vim.opt.rtp:prepend(lazypath)
 
-
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
 require("lazy").setup({
     spec = {
-        -- add your plugins here
+        -- import your plugins
+        { import = "config.plugins" },
     },
     -- Configure any other settings here. See the documentation for more details.
-    -- colorscheme that will be used when installing plugins.
-    install = { colorscheme = { "habamax" } },
-    -- automatically check for plugin updates
-    checker = { enabled = true },
 })
