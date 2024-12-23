64187d697646f107b08710e5a4da4435a910408c
Murray Silber <murraysilber@gmail.com>
2024-12-23 14:12:32 +0200

    chore: fix telescope config, missing args

--- apps/nvim/lua/plugins/treesitter.lua
+++ apps/nvim/lua/plugins/treesitter.lua
@@ -4,9 +4,6 @@ return {
     build = ":TSUpdate",
     config = function()
       require 'nvim-treesitter.configs'.setup {
-        -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
-
 
         -- Automatically install missing parsers when entering buffer
         -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
@@ -15,6 +12,10 @@ return {
         highlight = {
           enable = true,
 
+          -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+          -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+          -- the name of the parser)
+          -- list of language that will be disabled
           -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
           disable = function(lang, buf)
             local max_filesize = 100 * 1024 -- 100 KB
