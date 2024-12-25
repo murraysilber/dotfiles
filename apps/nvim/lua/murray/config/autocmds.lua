21afec76cbfcfc9c5a198e234e0a683a46617916
Murray Silber <murraysilber@gmail.com>
2024-12-25 19:46:00 +0200

    feat: ruff lsp, mason for easy lsp installs and management

--- apps/nvim/lua/murray/config/autocmds.lua
+++ apps/nvim/lua/murray/config/autocmds.lua
@@ -2,9 +2,25 @@
 -- Try it with yap in Normal mode
 -- See ':help vim.highlight.on._yank()'
 vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = "highlight when yanking (copying) text",
-    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-    callback = function()
-        vim.highlight.on_yank()
-    end,
+  desc = "highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- 1
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("lsp", { clear = true }),
+  callback = function(args)
+    -- 2
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      -- 3
+      buffer = args.buf,
+      callback = function()
+        -- 4 + 5
+        vim.lsp.buf.format { async = false, id = args.data.client_id }
+      end,
+    })
+  end
 })
--- apps/nvim/lua/murray/config/keymaps.lua
+++ apps/nvim/lua/murray/config/keymaps.lua
@@ -1,14 +1,22 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+local keymap = vim.keymap -- for conciseness
+
 -- keymap for netRw
-vim.keymap.set("n", "<space>pv", vim.cmd.Ex)
+keymap.set("n", "<space>pv", vim.cmd.Ex)
 
 -- source the current file
-vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 
 -- source the current line
-vim.keymap.set("n", "<space>x", ":.lua<CR>")
+keymap.set("n", "<space>x", ":.lua<CR>")
 
 -- source selected lines (Visual mode)
-vim.keymap.set("v", "<space>x", ":lua<CR>")
+keymap.set("v", "<space>x", ":lua<CR>")
+
+-- window management
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })     -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })   -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })      -- make split windows equal width & height
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
