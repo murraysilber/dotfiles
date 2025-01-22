87ee3590f8a1130e169fe696ffdbc2f8c47ad849
Murray Silber <murraysilber@gmail.com>
2025-01-22 10:09:45 +0200

    refactor: get bootstrap working with stow and new approach

--- scripts/.config/scripts/bootstrap.sh
+++ scripts/.config/scripts/bootstrap.sh
@@ -7,7 +7,7 @@
 # 4. And also installs other Apps using Homebrew
 ############################
 
-# util for user friendly output
+# util functions for user friendly output
 default_color=$(tput sgr 0)
 red="$(tput setaf 1)"
 yellow="$(tput setaf 3)"
@@ -30,19 +30,29 @@ warning() {
     printf "%s==> %s%s\n" "$yellow" "$1" "$default_color"
 }
 
+# Install x-code cli tools. (Needed for homebrew and git installs)
+install_xcode() {
+    info "Installing Apple's CLI tools (prerequisites for Git and Homebrew)..."
+    if xcode-select -p >/dev/null; then
+        warning "xcode is already installed"
+    else
+        xcode-select --install
+        sudo xcodebuild -license accept
+    fi
+}
+
 # Install Homebrew
-# Check if Homebrew has already been installed. 
 # Make the install non-interactive using NONINTERACTIVE=1
 install_homebrew() {
     info "Installing Homebrew..."
     # `command -v brew` will output nothing if Homebrew is not installed
-	  if [[ $(command -v brew) == "" ]]; then
-      sudo --validate
-      NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-      eval "$(/opt/homebrew/bin/brew shellenv)"
-	  else
-      warning "Homebrew already installed"
-	  fi
+    if [[ $(command -v brew) == "" ]]; then
+        sudo --validate
+        NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        eval "$(/opt/homebrew/bin/brew shellenv)"
+    else
+        warning "Homebrew already installed"
+    fi
 }
 
 # dotfiles directory
@@ -50,61 +60,58 @@ dotfile_dir="${HOME}/dotfiles"
 # DOTFILES_DIR="$HOME/Repos/github.com/mischavandenburg/dotfiles"
 XDG_CONFIG_HOME="$HOME/.config"
 
-# Install Homebrew - First check if Homebrew has been installed.
-# This will also install X-code tools as part of the Homebrew installation
-# We need X-code tools for a number of things
-install_homebrew()
-
-# Test if Homebrew was installed
-  brew --version
-
 # clone dotfiles repo from github
-if [[ -d "$dotfile_dir" ]]; then
-  echo "Dotfiles Directory exists"
-  echo "Checking if git repo exists....."
-  # Check if a git repo exists in the Dotfiles directory
-  if git rev-parse --git-dir >/dev/null 2>&1; then
-    # This is a valid git repository (but the current working
-    # directory may not be the top level.
-    # Check the output of the git rev-parse command if you care)
-    echo .git
-    echo "Git repo exists"
-    # TODO get input from user and based on input, determine if we exit here or not
-    echo "Should you rather be running the update script?......"
-    exit 1
-  fi
-else
-  echo "Cloning git repo...."
-  git clone -b main https://github.com/murraysilber/dotfiles.git "$dotfile_dir"
-  if [[ $? -eq 0 ]]; then
-    echo "Repo cloned successfully"
-  else
-    echo "Repo was not cloned successfully"
-    echo "Script will now exit..."
-    exit 1
-  fi
-fi
+clone_git_repo() {
+    if [[ -d "$dotfile_dir" ]]; then
+        info "Dotfiles Directory exists"
+        info "Checking if git repo exists....."
+        # Check if a git repo exists in the Dotfiles directory
+        if git rev-parse --git-dir >/dev/null 2>&1; then
+            # This is a valid git repository (but the current working
+            # directory may not be the top level.
+            # Check the output of the git rev-parse command if you care)
+            warning "Git repo exists"
+            # TODO: get input from user and based on input, determine if we exit here or not
+            warning "Should you rather be running the update script?......"
+            exit 1
+        fi
+    else
+        info "Cloning git repo...."
+        git clone -b main https://github.com/murraysilber/dotfiles.git "$dotfile_dir"
+        if [[ $? -eq 0 ]]; then
+            info "Repo cloned successfully"
+        else
+            error "Repo was not cloned successfully"
+            error "Script will now exit..."
+            exit 1
+        fi
+    fi
+}
 
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-if [[ ! -d "$XDG_CONFIG_HOME" ]]; then
-  echo "$XDG_CONFIG_HOME" does not exist.
-  echo Creating "$XDG_CONFIG_HOME"
-  mkdir -v "$XDG_CONFIG_HOME"
-fi
+install_xcode
+install_homebrew
+clone_git_repo
+
+# if [[ ! -d "$XDG_CONFIG_HOME" ]]; then
+#     echo "$XDG_CONFIG_HOME" does not exist.
+#     echo Creating "$XDG_CONFIG_HOME"
+#     mkdir -v "$XDG_CONFIG_HOME"
+# fi
 
 # Are we in the dotfiles directory?
-cd "${HOME}"/dotfiles
+#cd "${HOME}"/dotfiles
 
-# Run installers
-for i in wallpaper macos zsh wezterm tmux shell-tools rectangle vscode mise uv apps; do
-  echo "$HOME/dotfiles/$i/setup.sh"
-  source "$HOME/dotfiles/$i/setup.sh"
-  # cd $i && ./setup.sh
-  # cd -
-done
-
+# # Run installers
+# for i in wallpaper macos zsh wezterm tmux shell-tools rectangle vscode mise uv apps; do
+#     echo "$HOME/dotfiles/$i/setup.sh"
+#     source "$HOME/dotfiles/$i/setup.sh"
+#     # cd $i && ./setup.sh
+#     # cd -
+# done
+#
 # INSTALL APPS
 # Install Apps using Homebrew & mas
 # echo "Installing apps using Homebrew and mas"
