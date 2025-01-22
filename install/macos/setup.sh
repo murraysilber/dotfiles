ec28f889a0c38b9d8f4c2312e7c4c55bd4d2c17f
Murray Silber <murraysilber@gmail.com>
2025-01-22 09:16:51 +0200

    chore: clean up unused scripts

--- install/macos/setup.sh
+++
@@ -1,8 +0,0 @@
-# Update macOS defaults and wallpaper
-for i in wallpaper defaults; do
-  echo "$HOME/dotfiles/macos/$i/setup.sh"
-  # shellcheck disable=SC1090
-  source "$HOME/dotfiles/macos/$i/setup.sh"
-  # cd $i && ./setup.sh
-  # cd -
-done
