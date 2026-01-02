1b260d3d350afb873a1c110432d6094fd26f9210
Murray Silber <murraysilber@gmail.com>
2026-01-02 12:04:01 +0200

    Alternative Gruvbox option

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -5,6 +5,7 @@ return {
   config = function()
     -- Optionally configure and load the colorscheme
     -- directly inside the plugin declaration.
+    -- Do I want to test out another Gruvbox theme?
     vim.g.gruvbox_material_enable_italic = true
     vim.cmd.colorscheme('gruvbox-material')
   end
